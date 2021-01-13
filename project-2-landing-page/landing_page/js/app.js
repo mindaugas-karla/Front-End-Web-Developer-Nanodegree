45877d212334d3659ca131414df594dd3d2a0a46
Mindaugas Karla <mindaugaskarla@gmail.com>
2021-01-13 17:19:24 +0200

    completed all tasks

--- project-2-landing-page/landing_page/js/app.js
+++ project-2-landing-page/landing_page/js/app.js
@@ -14,8 +14,6 @@
 */
 
 
-
-
 /**
 TASKS:
     [x] Navigation is built dynamically as an unordered list.
@@ -27,12 +25,9 @@ Suggestions:
     [x] Hide fixed navigation bar while not scrolling (it should still be present on page load).
     [x] Hint: setTimeout can be used to check when the user is no longer scrolling.
     [x] Add a scroll to top button on the page that’s only visible when the user scrolls below the fold of the page.
-    [] Update/change the design/content.
+    [x] Update/change the design/content. +-
     [x] Make sections collapsible.
  */
-
-
-
 
 
 /**
@@ -40,14 +35,15 @@ Suggestions:
  * 
 */
 history.scrollRestoration = "manual";
+
 const rootElement = document.documentElement;
 const navigationHeader = document.querySelector('.page__header');
-
 const navigationBar = document.getElementById('navbar__list');
 const navigationMenuLink = document.querySelectorAll('menu__link');
 const sectionsAll = document.querySelectorAll('section');
-let currentActive = false;
 
+let currentActive = false;
+let timerForHeaderDisplay;
 
 /**
  * End Global Variables
@@ -74,100 +70,18 @@ function removeActiveClass(section, className) {
     section.style.cssText = "";
 }
 
-
 function showScroller() {
-    // Do something on scroll
     const scrollUpId = document.getElementById("scrollerToTop");
     var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
     if ((rootElement.scrollTop / scrollTotal) > 0.3) {
-        // Show button
+        //show button
         scrollUpId.classList.remove("scroll-hide");
     }
     else {
-        // Hide button
+        //hide button
         scrollUpId.classList.add("scroll-hide");
     }
 }
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-function createNavigationBar() {
-    const navigationStack = document.createDocumentFragment();
-
-    for (const section of sectionsAll) {
-        const sectionId = section.id;
-        const sectionName = section.dataset.nav;
-
-        const listTab = document.createElement('li');
-        const tabLink = document.createElement('a');
-
-        tabLink.innerText = sectionName;
-        tabLink.classList = 'menu__link';
-        tabLink.href = `#${sectionId}`;
-        tabLink.id = `${sectionId}_link`;
-        tabLink.dataset.idas= `${sectionId}`;
-
-        tabLink.onclick = function() { toggleSection(this, 'link') };
-
-
-        
-
-        listTab.appendChild(tabLink);
-        navigationStack.appendChild(listTab);
-    }
-    navigationBar.appendChild(navigationStack);
-}
-
-// Add class 'active' to section when near top of viewport
-
-function checkActiveView() {
-    for (const section of sectionsAll) {
-        const sectionId = section.id;
-        const updateMenuTab = `${sectionId}_link`;
-
-        if (checkViewport(section)) {
-            currentActive = sectionId;
-            addActiveClass(section, 'your-active-class');
-
-            addActiveStateLink(updateMenuTab);
-        }
-        else {
-            if (currentActive !== section.id) {
-                removeActiveClass(section, 'your-active-class');
-                removeActiveStateLink(updateMenuTab);
-            }
-
-        }
-    }
-}
-
-
-// Scroll to anchor ID using scrollTO event
-
-function changeScrollBehavior() {
-    // scroll into view
-    const selectHTML = document.querySelector('html');
-    selectHTML.style.cssText = "scroll-behavior: smooth;";
-}
-
-function scrollToTopPage() {
-    const pageHref = window.location.href;
-    if (pageHref.indexOf('#')) {
-        var noHashURL = pageHref.replace(/#.*$/, '');
-        window.history.replaceState('', document.title, noHashURL);
-    }
-    window.scrollTo(0, 0);
-}
-
-
-
-
 
 function addActiveStateLink(current) {
     const currentTab = document.getElementById(current);
@@ -176,74 +90,11 @@ function addActiveStateLink(current) {
 }
 
 function removeActiveStateLink(current) {
-    //for (const tab of navigationMenuLink) {
-    //if (tab.id != current) {
     const currentTab = document.getElementById(current);
-
     currentTab.classList.remove("active");
-    // }
-    //}
 }
 
-
-//function menuLinkActiveTab
-
-function createScroller() {
-    const scrollerDiv = document.createElement('div');
-    scrollerDiv.id = "scrollerToTop";
-    scrollerDiv.classList = "scroll-element scroll-hide noselect pointerCursor";
-    scrollerDiv.onclick = function () {scrollToTopPage()};
-    const scrollerSpan = document.createElement('span');
-    scrollerSpan.innerText = "UP";
-    scrollerDiv.appendChild(scrollerSpan);
-
-    const applyToHeader = document.querySelector('header');
-
-    document.body.appendChild(scrollerDiv);
-
-}
-
-createScroller();
-
-
-
-function disableScrollbar() {
-    setTimeout(function () {
-        //document.body.style.overflow = 'hidden';
-
-        body.classList.add("hide-scroll-bar");
-    }, 2000);
-}
-
-function enableScrollbar() {
-    body.classList.remove("hide-scroll-bar");
-}
-
-
-
-/**
- * End Main Functions
- * Begin Events
- *
-*/
-changeScrollBehavior();
-
-// Build menu 
-createNavigationBar();
-
-// Scroll to section on link click
-
-let timerForHeaderDisplay;
-
-function showMenu () {
-    navigationHeader.classList.remove("hide-menu-bar");
-    clearTimeout(timerForHeaderDisplay);
-    timerForHeaderDisplay = setTimeout(function () {
-        navigationHeader.classList.add("hide-menu-bar");
-    }, 2500);
-}
-
-function toggleSection (test, type) {
+function toggleSection(test, type) {
     const secId = test.dataset.idas;
     const sectionCollapse = document.getElementById(secId);
     const statuschange = sectionCollapse.querySelectorAll('span')[1];
@@ -263,13 +114,106 @@ function toggleSection (test, type) {
             statuschange.innerText = " - Opened";
         }
     }
-    
-
-
 }
-function createSectionCollabsibleBar() {
-    //const customisedSection = document.createDocumentFragment();
 
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// Build the Navigation
+function createNavigationBar() {
+    const navigationStack = document.createDocumentFragment();
+
+    for (const section of sectionsAll) {
+        const sectionId = section.id;
+        const sectionName = section.dataset.nav;
+
+        const listTab = document.createElement('li');
+        const tabLink = document.createElement('a');
+
+        tabLink.innerText = sectionName.toUpperCase();
+        tabLink.classList = 'menu__link noselect pointerCursor';
+        tabLink.href = `#${sectionId}`;
+        tabLink.id = `${sectionId}_link`;
+        tabLink.dataset.idas = `${sectionId}`;
+
+        tabLink.onclick = function () { toggleSection(this, 'link') };
+
+        listTab.appendChild(tabLink);
+        navigationStack.appendChild(listTab);
+    }
+    navigationBar.appendChild(navigationStack);
+}
+
+// Add class 'active' to section when near top of viewport
+
+function checkActiveView() {
+    for (const section of sectionsAll) {
+        const sectionId = section.id;
+        const updateMenuTab = `${sectionId}_link`;
+
+        if (checkViewport(section)) {
+            currentActive = sectionId;
+            if (!section.classList.contains("collapse-section")) {
+                addActiveClass(section, 'your-active-class');
+            }
+            addActiveStateLink(updateMenuTab);
+        }
+        else {
+            if (currentActive !== section.id) {
+                removeActiveClass(section, 'your-active-class');
+                removeActiveStateLink(updateMenuTab);
+            }
+        }
+    }
+}
+
+// Changing html begaviour
+function changeScrollBehavior() {
+    // scroll into view
+    // testing udacity lesson information
+    const selectHTML = document.querySelector('html');
+    selectHTML.style.cssText = "scroll-behavior: smooth;";
+}
+
+// Scroll to top button on the page
+function scrollToTopPage() {
+    const pageHref = window.location.href;
+    if (pageHref.indexOf('#')) {
+        var noHashURL = pageHref.replace(/#.*$/, '');
+        window.history.replaceState('', document.title, noHashURL);
+    }
+    window.scrollTo(0, 0);
+}
+
+// Build Scroller
+function createScroller() {
+    const scrollerDiv = document.createElement('div');
+    scrollerDiv.id = "scrollerToTop";
+    scrollerDiv.classList = "scroll-element scroll-hide noselect pointerCursor";
+    scrollerDiv.onclick = function () { scrollToTopPage() };
+    const scrollerSpan = document.createElement('span');
+    scrollerSpan.innerText = "UP";
+    scrollerDiv.appendChild(scrollerSpan);
+
+    const applyToHeader = document.querySelector('header');
+
+    document.body.appendChild(scrollerDiv);
+}
+
+// Build Hide Menu Option
+function showMenu() {
+    navigationHeader.classList.remove("hide-menu-bar");
+    clearTimeout(timerForHeaderDisplay);
+    timerForHeaderDisplay = setTimeout(function () {
+        navigationHeader.classList.add("hide-menu-bar");
+    }, 2500);
+}
+
+// Build Collabsible
+function createSectionCollabsibleBar() {
     for (const section of sectionsAll) {
         const sectionId = section.id;
         const sectionName = section.dataset.nav;
@@ -277,41 +221,41 @@ function createSectionCollabsibleBar() {
         const titleCustom = section.querySelector('h2');
         titleCustom.dataset.idas = sectionId;
         titleCustom.classList.add("pointerCursor");
-        titleCustom.onclick = function() { toggleSection(this, 'section') };
-
-        //titleCustom.addEventListener("click", (event) => {
-            //section.classList.toggle("collapse-section");
-           //const name = section.dataset.nav;
-            //console.log(event.dataset.idas);
-
-                   //titleCustom.
-//alert(123);
-         // });
-
-
-
+        titleCustom.onclick = function () { toggleSection(this, 'section') };
 
         titleCustom.innerHTML = "";
 
         const spanName = document.createElement('span');
         spanName.innerText = sectionName;
-        
+
         const spanCollapse = document.createElement('span');
         spanCollapse.innerText = ' - Opened';
-        
+
         titleCustom.appendChild(spanName);
         titleCustom.appendChild(spanCollapse);
-
-        //customisedSection.appendChild(titleCustom);
-        //`<span>${sectionName}</span> <span class="collapse_section">Collapse</span>`;
     }
-
 }
-createSectionCollabsibleBar ();
 
-// Set sections as active
+/**
+ * End Main Functions
+ * Begin Events
+ *
+*/
+// Change Behaviour for Smooth Anchor Scrolling
+changeScrollBehavior();
+
+// Create Menu 
+createNavigationBar();
+
+// Activate Scroller
+createScroller();
+
+// Activate Collabsible Future
+createSectionCollabsibleBar();
+
+// Activate Scrolling Monitor
 window.addEventListener('scroll', checkActiveView);
 
-
+// Activate Menu Hide Than not scrolling
 window.addEventListener('scroll', showMenu);
 
