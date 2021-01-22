6c3dd8eb06cd5c5d320afd619b8fd92210360ef0
Mindaugas Karla <mindaugaskarla@gmail.com>
2021-01-22 14:59:39 +0200

    prep

--- project-3-weather-journal-app/weather-journal-app/server.js
+++ project-3-weather-journal-app/weather-journal-app/server.js
@@ -1,31 +1,59 @@
-// Setup empty JS object to act as endpoint for all routes
+/* Server & Routes Setup */
+
 // Express to run server and routes
-console.log('test');
 const express = require('express');
+
 // Start up an instance of app
 const app = express();
+
 /* Dependencies */
 const bodyParser = require('body-parser');
+
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 // Cors for cross origin allowance
 const cors = require('cors');
 app.use(cors());
+
 // Initialize the main project folder
 app.use(express.static('/website'));
 
-const port = 8000;
+// Set port
+const port = 1337;
+
 // Spin up the server
 const server = app.listen(port, listening);
+
 // Callback to debug
-function listening(){
+function listening() {
     console.log('server running');
     console.log(`running on localhost: ${port}`);
 };
-// Initialize all route with a callback function
+
+// Setup empty JS object to act as endpoint for all routes
+const projectData = [];
 
 // Callback function to complete GET '/all'
+app.get('/all', sendData);
+
+function sendData(req, res) {
+    res.send(projectData);
+}
 
 // Post Route
+app.post('/addData', addData);
+
+function addData(req, res) {
+    newEntry = {
+        temperature: req.body.temperature,
+        date: req.body.date,
+        feelings: req.body.feelings,
+    };
+
+    projectData.push(newEntry);
+    res.end();
+}
+
--- project-3-weather-journal-app/weather-journal-app/website/app.js
+++ project-3-weather-journal-app/weather-journal-app/website/app.js
@@ -1,3 +1,13 @@
+/* Global Variables */
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = 'b0a261c59f09156790ca4d47525c070a';
+
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
 // Personal API Key for OpenWeatherMap API
 
 // Event listener to add function to existing HTML DOM element
