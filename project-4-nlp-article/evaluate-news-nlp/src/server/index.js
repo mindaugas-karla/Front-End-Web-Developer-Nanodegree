4215bf3e7a9c30a31861a1eeb7a2a177a9e89c68
Mindaugas Karla <mindaugaskarla@gmail.com>
2021-03-27 18:50:58 +0200

    change stuff

--- project-4-nlp-article/evaluate-news-nlp/src/server/index.js
+++ project-4-nlp-article/evaluate-news-nlp/src/server/index.js
@@ -16,13 +16,14 @@ app.use(express.static('dist'))
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
+    res.sendFile()
 })
 
+
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8088, function () {
+    console.log('Example app listening on port 8088!')
 })
 
 app.get('/test', function (req, res) {
