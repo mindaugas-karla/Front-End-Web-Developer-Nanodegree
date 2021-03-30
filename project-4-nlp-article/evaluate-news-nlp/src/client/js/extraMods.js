12e57df3a7fb7aa42d4dd598151f9b8991ad7a0d
Mindaugas Karla <mindaugaskarla@gmail.com>
2021-03-30 16:06:13 +0300

    final step

---
+++ project-4-nlp-article/evaluate-news-nlp/src/client/js/extraMods.js
@@ -0,0 +1,13 @@
+
+
+
+
+function checkInputValue (response) {
+
+
+
+
+
+
+
+}
--- project-4-nlp-article/evaluate-news-nlp/src/client/js/formHandler.js
+++ project-4-nlp-article/evaluate-news-nlp/src/client/js/formHandler.js
@@ -2,7 +2,10 @@ function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
+    let formText = document.getElementById('name').value;
+
+
+    
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
