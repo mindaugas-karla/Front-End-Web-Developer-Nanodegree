e385219836207014c7b89f1cef8fbf2723ee4459
Mindaugas Karla <mindaugaskarla@gmail.com>
2021-04-01 18:02:40 +0300

    workers to dev also

--- project-4-nlp-article/evaluate-news-nlp/webpack.dev.js
+++ project-4-nlp-article/evaluate-news-nlp/webpack.dev.js
@@ -1,7 +1,9 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 
 module.exports = {
     mode: 'development',
@@ -47,6 +49,7 @@ module.exports = {
             // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
-        })
+        }),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
--- project-4-nlp-article/evaluate-news-nlp/webpack.prod.js
+++ project-4-nlp-article/evaluate-news-nlp/webpack.prod.js
@@ -1,7 +1,7 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
